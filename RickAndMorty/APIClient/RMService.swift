0aec32ced31515d6f8ef2d35be19f2c8a5e6374c
Fatihan Ziyan <ziyanfatihan@gmail.com>
2024-02-26 11:10:12 +0300

    implement APICache manager

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -12,6 +12,8 @@ final class RMService{
     
     static let shared = RMService()
     
+    private let cacheManager = RMAPICacheManager()
+    
     private init(){}
     
     enum RMServiceError: Error {
@@ -23,12 +25,24 @@ final class RMService{
     public func execute<T: Codable>(_ request: RMRequest, expecting type: T.Type,
                         completion: @escaping (Result<T, Error>) -> Void)
     {
+        if let cachedData = cacheManager.cachedResponse(for: request.endpoint, url: request.url) {
+            print("Using cached API Response")
+            do {
+                let result = try JSONDecoder().decode(type.self, from: cachedData)
+                completion(.success(result))
+            }
+            catch{
+                completion(.failure(error))
+            }
+            return
+        }
+        
         guard let urlRequest = self.request(from: request) else {
             completion(.failure(RMServiceError.failedToCreatedRequest))
             return
         }
         //print("API Call: \(request.url?.absoluteString ?? "")")
-        let task = URLSession.shared.dataTask(with: urlRequest) { data, _ ,error in
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _ ,error in
             guard let data = data, error == nil else{
                 completion(.failure(error ?? RMServiceError.failedToGetData))
                 return
@@ -37,6 +51,7 @@ final class RMService{
             // Decode Response
             do{
                 let result = try JSONDecoder().decode(type.self, from: data)
+                self?.cacheManager.setCache(for: request.endpoint, url: request.url, data: data)
                 completion(.success(result))
             }
             catch{
